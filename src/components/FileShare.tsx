53a2e33c21458edd751298d31fb0cefb88697b26
mokshith-moksh <mokshithmokshith7@gmail.com>
2025-08-21 16:09:37 +0530

    Updated-links

--- src/components/FileShare.tsx
+++ src/components/FileShare.tsx
@@ -361,7 +361,7 @@ const FileShare: React.FC<FileShareProps> = ({ files }) => {
                 <Input
                   ref={urlRef}
                   className="h-10 w-full rounded-lg bg-white px-2 text-sm text-black md:w-[85%] md:text-lg"
-                  value={`https://shipfilezz-iuhj.vercel.app/${shareCode}`}
+                  value={`https://shipfilezz-iuhj.vercel.app/receiver?code=${shareCode}`}
                   readOnly
                 />
                 <button
